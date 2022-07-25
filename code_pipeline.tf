9184f27078ef7da73fed55e18992d19d90adec9b
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-25 16:57:22 -0700

    Update code_pipeline.tf

--- code_pipeline.tf
+++ code_pipeline.tf
@@ -1,13 +1,6 @@
 resource "aws_s3_bucket" "pipeline" {
   bucket = "${var.name}-codepipeline-bucket"
   tags = var.tags
-  
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = "AES256"
-      }
-    }
   }
 
  resource "aws_s3_bucket_server_side_encryption_configuration" "this" {

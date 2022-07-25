8045d6d6e0499d3583ab94a3892de259d497d0e2
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-25 15:13:59 -0700

    Update code_deploy.tf

--- code_deploy.tf
+++ code_deploy.tf
@@ -104,7 +104,7 @@ resource "aws_codedeploy_deployment_group" "this" {
   load_balancer_info {
     target_group_pair_info {
       prod_traffic_route {
-        listener_arns = [var.listener_arns]
+        listener_arns = var.listener_arns
       }
 
       target_group {

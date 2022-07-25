5e37dea69a86a6ad25a75f977fe76fa7508ba1c1
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-25 13:37:39 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -23,3 +23,8 @@ variable "aws_account_id" {
 variable "region" {
   default = "us-west-2"
 }
+variable "tags" {
+  default     = {}
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+}

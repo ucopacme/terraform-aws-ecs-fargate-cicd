1bbe2eeb367b022c299f0544937c4eb8aa4b0176
George Holbert <gholbert@ucop.edu>
2024-03-09 18:16:21 -0800

    (fixed typo) added codebuild_name, codebuild_description variables

--- code_build.tf
+++ code_build.tf
@@ -132,7 +132,7 @@ resource "aws_iam_role_policy" "codebuild" {
 
 resource "aws_codebuild_project" "this" {
   name         = "${local.codebuild_name}-codebuild"
-  description  = local.description
+  description  = local.codebuild_description
   service_role = aws_iam_role.codebuild.arn
 
   artifacts {
